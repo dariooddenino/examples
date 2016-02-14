9fe6422ad8bc611ca832f1b08818df7ec55e3e91
Max Gerlach <m@px0.de>
2016-02-13 23:35:04 -0500

    Refactor

--- src/examples/core.clj
+++ src/examples/core.clj
@@ -1,38 +1,49 @@
 (ns examples.core
   (:require [clojure.string :as str]
-            [hickory.select :as s])
-  (use clj-webdriver.taxi)
-  (use hickory.core)
+            [hickory.select :as s]
+            [hickory.core :refer :all]
+            [clj-webdriver.taxi :refer :all]
+            [clj-webdriver.driver :refer [init-driver]])
+  
+  ;;PhantomJS is necessary because the examples get dynamically injected
   (import org.openqa.selenium.phantomjs.PhantomJSDriver
-          org.openqa.selenium.remote.DesiredCapabilities)
-  (use [clj-webdriver.driver :only [init-driver]]))
+          org.openqa.selenium.remote.DesiredCapabilities))
 
 (set-driver! (init-driver {:webdriver (PhantomJSDriver. (DesiredCapabilities.))}))
 
 (def cache (atom {}))
 
+(defn cached
+  [fun key]
+  (if-let [hit (get @cache key)]
+      hit
+      (let [result (fun key)]
+        (swap! cache assoc key result)
+        result)))
+
 (defn build-url
   [vname]
   (str "https://clojuredocs.org/" vname))
 
-(defn clean
-  [fname]
-  (last (re-find #"#'(.*)" (str fname))))
+(defn extract-examples [body]
+  (apply str
+         (s/select
+          (s/descendant (s/class "syntaxify")
+                        (s/not (s/tag :span)))
+          body)))
 
+(defn get-examples [ex]
+  (do (-> ex
+          build-url
+          to)
+      (-> (html "body")
+          parse
+          as-hickory
+          extract-examples)))
 
 (defmacro examples
   [name]
-  `(let [x# (clean (resolve '~name))]
-     (if (not (nil? (get-in @cache [x#])))
-       (get-in @cache [x#])
-       (do (to (build-url x#))
-           (let [result# (as-hickory (parse (html "body")))
-                 parsed# (apply str
-                                (s/select
-                                  (s/descendant (s/class "syntaxify")
-                                                (s/not (s/tag :span)))
-                                  result#))]
-             (do (swap! cache assoc-in [x#] parsed#)
-                 parsed#))))))
-
-(examples not-empty)
+  `(let [clean# #(->> (str %) (re-find #"#'(.*)") last)
+         ex# (-> '~name resolve clean#)
+         examples# (cached get-examples ex#)]
+     examples#))
